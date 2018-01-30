25a0207b6f6daea5ac8ac3c10e20bd6d45167d1d
Daniel Schep <dschep@gmail.com>
2018-01-29 20:08:20 -0500

    doh.. apparently kms aliases are no good. also switch to middys ssm

--- handler.js
+++ handler.js
@@ -2,23 +2,10 @@
 
 const fetch = require('node-fetch');
 const middy = require('middy');
+const {ssm} = require('middy/middlewares');
 const turf = require('@turf/turf');
 const AWS = require('aws-sdk');
 
-
-const ssm = new AWS.SSM({region: process.env.AWS_REGION && process.env.AWS_REGION !== 'undefined' ? process.env.AWS_REGION : 'us-east-1'});
-const ssmParameterMiddleware =  (parameters, decrypt = true) => ({
-  before: (handler) => {
-    return ssm.getParameters({
-      Names: parameters.map(param => `/${process.env.SERVICE_NAME}/${process.env.STAGE}/${param}`),
-      WithDecryption: decrypt,
-    }).promise()
-    .then(({Parameters}) => {
-      handler.context.parameters = Parameters.reduce(
-        (params, {Name, Value}) => Object.assign(params, {[Name.split('/')[3]]: Value}), {});
-    });
-  },
-})
 
 const home = {
   type: 'Feature',
@@ -49,8 +36,8 @@ const jumpstart = (event, context) => fetch('https://dc.jumpmobility.com/opendat
   })
   .then(nearestBike => {
     const params = {
-      token: context.parameters.pushover_api_token,
-      user: context.parameters.pushover_user_key,
+      token: context.pushoverApiToken,
+      user: context.pushoverUserKey,
       message: `The nearest JUMP bike is ${nearestBike.properties.distance.toFixed(2)}mi away.`,
       url: `https://www.google.com/maps?q=${nearestBike.coordinates[1]},+${nearestBike.coordinates[0]}`,
       url_title: 'View in Google Maps',
@@ -64,5 +51,11 @@ const jumpstart = (event, context) => fetch('https://dc.jumpmobility.com/opendat
   })
 
 const handler = middy(jumpstart);
-handler.use(ssmParameterMiddleware(['pushover_api_token', 'pushover_user_key']));
+handler.use(ssm({
+  params: {
+    pushoverApiToken: `/${process.env.SERVICE_NAME}/${process.env.STAGE}/pushover_api_token`,
+    pushoverUserKey: `/${process.env.SERVICE_NAME}/${process.env.STAGE}/pushover_user_key`,
+  },
+  setToContext: true,
+}));
 module.exports.jumpstart = handler
